8b1375f2d3bae72b6354d9bc73a106681740c56b
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-03-28 21:03:24 -0400

    update to server

--- Develop/server.js
+++ Develop/server.js
@@ -11,3 +11,30 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
  
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+
+app.post('/api/notes', (req, res) => {
+  const {title, text} = req.body;
+ 
+  if (title && text) {
+    const newInput = {
+      title,
+      text,
+    };
+    readAndAppend(newInput, './db/db.json');
+    const response = {
+      status: 'success',
+      body: newInput,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
