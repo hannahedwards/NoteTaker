b6ec736cf354e59d218779a234f22bbd2ce84954
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-03-28 23:08:58 -0400

    added comments

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
 app.use(express.static('public')); 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
@@ -15,26 +14,26 @@ app.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {//creates new note
   const {title, text} = req.body;
  
-  if (title && text) {
+  if (title && text) {//title it note title and text is the note body
     const newInput = {
       title,
       text,
     };
-    readAndAppend(newInput, './db/db.json');
+    readAndAppend(newInput, './db/db.json');//note added
     const response = {
       status: 'added',
       body: newInput,
     };
 
     res.json(response);
-  } else {
+  } else {//note error
     res.json('try again');
   }
 });
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+app.listen(PORT, () =>//triggers app to run
+  console.log(`App listening at http://localhost:${PORT}`)
 );
